60a1ffed00651ea1e6a6bec2200ab7e51481d690
Matias <Matias03gonzalez@gmail.com>
2026-01-29 00:33:18 -0300

    chore: critical fix for EAS monorepo entry point and metro config

--- metro.config.js
+++ metro.config.js
@@ -1,15 +1,16 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const path = require('path');
 
-const projectRoot = __dirname;
-const workspaceRoot = path.resolve(projectRoot, '..');
+// 1. Find the project and workspace directories
+const projectRoot = path.resolve(__dirname, 'frontend');
+const workspaceRoot = __dirname;
 
 const config = getDefaultConfig(projectRoot);
 
-// 1. Watch all files in the monorepo
+// 2. Watch all files in the monorepo
 config.watchFolders = [workspaceRoot];
 
-// 2. Let Metro look in the root node_modules as well
+// 3. Let Metro look in the root node_modules and the frontend node_modules
 config.resolver.nodeModulesPaths = [
     path.resolve(projectRoot, 'node_modules'),
     path.resolve(workspaceRoot, 'node_modules'),
